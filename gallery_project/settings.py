87744b2c9acec9bcc60b1965991fcf955a614141
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-01-31 17:09:36 +0300

    Add a content to the template home

--- gallery_project/settings.py
+++ gallery_project/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'images.apps.ImagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'images.apps.ImagesConfig',
+ 
 ]
 
 MIDDLEWARE = [
