0caca320442a07c8d66d7392658f645cec83de99
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-03 00:26:34 +0300

    Add a model class for image

--- images/models.py
+++ images/models.py
@@ -37,3 +37,50 @@ class Category(models.Model):
     def update_category(cls, id, name):
         cls.objects.filter(id=id).update(name=name)
 
+
+class Image(models.Model):
+    image = models.ImageField(upload_to= 'images/')
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    location = models.ForeignKey(Location, default='')
+    category = models.ForeignKey(Category, default='')
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(image_id):
+        Image.objects.filter(id=image_id).delete()
+
+    @classmethod
+    def update_image(cls, id, name):
+        cls.objects.filter(id=id).update(name=name) 
+    
+    @classmethod
+    def get_image_by_id(cls, id):
+        try:
+            image = cls.objects.get(id=id)
+            print("Object found")
+            return image
+        except DoesNotExist:
+            print("object not found")
+
+    @classmethod
+    def filter_by_location(cls, location):
+        try:
+            images = cls.objects.filter(location=location)
+            return images
+        except DoesNotExist:
+            print('Objects do not exist')
+
+
+    @classmethod
+    def search_images(cls, category):
+        category = Category.objects.get(name=category)
+        images = cls.objects.filter(category=category.id)
+        return images
+
+
