4e554469ba370514dda0912f714a2964584f2eeb
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-03 00:23:32 +0300

    Add a model class for location

--- images/models.py
+++ images/models.py
@@ -1,7 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
-class Image(models.Model):
-    name = models.CharField(max_length =30)
-    image_name = models.charField(max_length=40)
-    image_description = models.charField(max_length=300)
+
+class Location(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def save_location(self):
+        self.save()
+
+    @classmethod
+    def delete_location(cls, id):
+        cls.objects.filter(id=id).delete()
+
+    @classmethod
+    def update_location(cls, id, name):
+        cls.objects.filter(id=id).update(name=name)
