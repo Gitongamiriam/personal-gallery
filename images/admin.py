0465be55624d61b2a94297404f1dceef3d63cd7b
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-03 00:10:47 +0300

    Add a class in the admin port

--- images/admin.py
+++ images/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Image
+from .models import Location, Image, Category
 
-# Register your models here.
+# Register your models here.Learn more or give us feedback
+
+admin.site.register(Location)
 admin.site.register(Image)
+admin.site.register(Category)
+
