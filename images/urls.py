219d521bba7cebec75c55df6d08d0d6cf4567043
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-03 00:19:44 +0300

    Add a url for search

--- images/urls.py
+++ images/urls.py
@@ -1,7 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
 
-urlpatterns=[
-    url('^$',views.home,name = 'home'),
+urlpatterns= [
+    url('^$', views.welcome, name = 'welcome'),
+    url('^search', views.search, name = 'search'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
     
