41ab3f308429121e25ecd11e8e2a8e10c21792b4
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-03 00:21:35 +0300

    Updated the views

--- images/views.py
+++ images/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Image, Category, Location
 
-# Create your views here.
-def home(request):
-    return render(request,'home.html')
+def welcome(request):
+    images = Image.objects.all()
+    categories = Category.objects.all()
+    locations = Location.objects.all()
+    return render(request, 'home.html', {'images':images, 'categories':categories, 'locations':locations})
 
+def search(request):
     
+    if 'category' in request.GET and request.GET["category"]:
+        category = request.GET.get("category")
+        images = Image.search_images(category)
+        message = f"{category}"
+
+        return render(request, 'search_results.html',{"message":message,"images": images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search_results.html',{"message":message})
