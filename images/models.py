795c23a2f360863e1574733980cd90d2ec1916cd
Gitongamiriam <miriamgitonga2019@gmail.com>
2020-02-02 13:18:45 +0300

    Restructure myfolders

--- images/models.py
+++ images/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Image(models.Model):
-    image_name = models.charField(max_length=40)
-    image_description = models.charField(max_length=300)
+# class Image(models.Model):
+#     name = models.CharField(max_length =30)
+#     image_name = models.charField(max_length=40)
+#     image_description = models.charField(max_length=300)
